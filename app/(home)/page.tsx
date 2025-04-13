26d96695e3ae4ca5cc7b41af4410fb99f6f225ef
heann <jse033101@gmail.com>
2025-04-13 20:03:44 +0900

    movie 영화 예고편 적용

--- app/(home)/page.tsx
+++ app/(home)/page.tsx
@@ -9,7 +9,6 @@ export const API_URL = "https://nomad-movies.nomadcoders.workers.dev/movies";
 
 async function getMovies() {
   //백엔드에서 fetch가 이뤄지므로 그때 사용자를 위한 UI가 없다. => loading.tsx이 나타남.
-  //await new Promise((resolve) => setTimeout(resolve, 1000));
   const response = await fetch(API_URL);
   const json = await response.json();
   return json;
--- app/(movies)/movies/[id]/page.tsx
+++ app/(movies)/movies/[id]/page.tsx
@@ -1,5 +1,4 @@
 import { Suspense } from "react";
-import { API_URL } from "../../../(home)/page";
 import MovieInfo from "../../../../components/movie-info";
 import MovieVideos from "../../../../components/movie-videos";
 
@@ -10,7 +9,6 @@ export default async function MovieDetail({
 }) {
   return (
     <div>
-      <h3>Movie Detail Page</h3>
       {/* Suspense는 await와 분리되어있음 */}
       {/* 페이지 전체가 로딩상태였지만 이제는 fetch해야하는 component만 로딩상태로 만들어줌. */}
       <Suspense fallback={<h1>Loading movie info</h1>}>
