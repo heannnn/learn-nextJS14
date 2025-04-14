fdccf520dfad790997b98e6f1e54bc63a269f6b4
DESKTOP-70JUD3O\안하은 <heAnn@inzisoft.com>
2025-04-14 15:54:19 +0900

    주석 적용

--- components/movie.tsx
+++ components/movie.tsx
@@ -18,6 +18,8 @@ export default function Movie({ title, id, poster_path }: IMovieProps) {
   return (
     <div className={styles.movie}>
       <img src={poster_path} alt={title} onClick={onClick} />
+      {/* prefetch를 적용함으로써 NextJS는 페이지들을 미리 로드하게 될것임 */}
+      {/* 스크롤을 내릴때마다 페이지에 들어가서 요청한것처럼 fetch함  */}
       <Link prefetch href={`/movies/${id}`}>
         {title}
       </Link>
